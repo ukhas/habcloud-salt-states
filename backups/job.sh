36f4cb8879dcb0bb610e79a5a58a4db82a9acbd7
Adam Greig <adam@adamgreig.com>
2015-02-15 16:03:59 +0000

    backup script should cd / first to avoid dull error messages

--- backups/job.sh
+++ backups/job.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Backup job {{ name }}
 
+# The user running the backup job probably can't operate from /root
+cd /
+
+# Run the specified command as the right user, nice and ioniced to avoid
+# unnecessarily loading the system, then pipe the result to our backup
+# pipeline which gzips, encrypts and uploads to S3.
 sudo -u {{ user }} nice ionice -c 3 {{ cmd }} |
 sudo -u backups    nice ionice -c 3 /home/backups/backup.sh \
     s3://{{ grains.id }}/{{ name }}-$(date +'%Y%m%dT%H%M%S').gz.gpg
