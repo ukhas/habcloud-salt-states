d02ccd948da1e2c8729f1c4b10729f7b2da629c8
Daniel Richman <main@danielrichman.co.uk>
2015-04-11 22:48:20 +0100

    install_dokuwiki: safer temp files

--- ukhas/install_dokuwiki.sh
+++ ukhas/install_dokuwiki.sh
@@ -3,13 +3,14 @@ set -xe
 TAR="tar --no-same-permissions --no-same-owner"
 
 mkdir /srv/ukhas-dokuwiki # must not exist already
+mkdir /tmp/dokuwiki
 
-rm -f /tmp/dokuwiki-stable.tgz /tmp/dokuwiki-recaptcha.tgz
-wget -qO /tmp/dokuwiki-stable.tgz    http://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
-wget -qO /tmp/dokuwiki-recaptcha.tgz https://github.com/liip/dw-plugin-recaptcha/archive/master.tar.gz
-$TAR -xf /tmp/dokuwiki-stable.tgz    -C /srv/ukhas-dokuwiki --strip-components=1
+wget -qO /tmp/dokuwiki/stable.tgz    http://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
+wget -qO /tmp/dokuwiki/recaptcha.tgz https://github.com/liip/dw-plugin-recaptcha/archive/master.tar.gz
+$TAR -xf /tmp/dokuwiki/stable.tgz    -C /srv/ukhas-dokuwiki --strip-components=1
 mkdir /srv/ukhas-dokuwiki/lib/plugins/recaptcha
-$TAR -xf /tmp/dokuwiki-recaptcha.tgz -C /srv/ukhas-dokuwiki/lib/plugins/recaptcha --strip-components=1
+$TAR -xf /tmp/dokuwiki/recaptcha.tgz -C /srv/ukhas-dokuwiki/lib/plugins/recaptcha --strip-components=1
+rm -rf /tmp/dokuwiki
 
 rm -rf /srv/ukhas-dokuwiki/data /srv/ukhas-dokuwiki/install.php
 ln -s /srv/ukhas-data/users.auth.php /srv/ukhas-dokuwiki/conf
