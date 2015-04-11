ba6d244015be429789275f1ee0cc6d0471991a78
Daniel Richman <main@danielrichman.co.uk>
2015-04-11 11:28:18 +0100

    ukhas config: baseurl, indexdelay

--- ukhas/conf/local.php
+++ ukhas/conf/local.php
@@ -3,13 +3,14 @@ $conf['title'] = 'UKHAS Wiki';
 $conf['tagline'] = 'UK High Altitude Society';
 $conf['license'] = '0';
 $conf['basedir'] = '/';
-$conf['baseurl'] = 'http://scratch.vm.habhub.org/';
+$conf['baseurl'] = 'http://ukhas.org.uk/';
 $conf['useacl'] = 1;
 $conf['superuser'] = '@admin';
 $conf['userewrite'] = '1';
 $conf['plugin']['recaptcha']['lang'] = 'en';
 $conf['plugin']['recaptcha']['editprotect'] = 0;
 $conf['mailfrom'] = 'dokuwiki@ukhas.org.uk';
+$conf['indexdelay'] = 0;
 
 define("DOKU_SESSION_NAME", "DOKUWIKI_SESSION");
 define("DOKU_COOKIE", "DOKUWIKI_AUTH");
