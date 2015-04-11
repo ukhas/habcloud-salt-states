7d5808e75ed07664969cb61cdfb0e72ced4796d5
Daniel Richman <main@danielrichman.co.uk>
2015-04-11 22:44:16 +0100

    ukhas incremental: also include a file list

--- ukhas/incremental_helper.sh
+++ ukhas/incremental_helper.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 touch /tmp/ukhas-dokuwiki-incremental-start
-tar c /srv/ukhas-data \
+find /srv/ukhas-data -type f > /tmp/ukhas-dokuwiki-file-list
+tar c /srv/ukhas-data /tmp/ukhas-dokuwiki-file-list \
     --newer /srv/ukhas-data/incremental-stamp \
     --exclude=/srv/ukhas-data/data/cache
 touch /srv/ukhas-data/incremental-stamp --reference=/tmp/ukhas-dokuwiki-incremental-start
-rm /tmp/ukhas-dokuwiki-incremental-start
+rm /tmp/ukhas-dokuwiki-incremental-start /tmp/ukhas-dokuwiki-file-list
