170de5f897869c457ffd47418c76779e6e1b2ae9
Daniel Richman <main@danielrichman.co.uk>
2015-04-11 22:37:10 +0100

    also exclude cache from incremental backups

--- ukhas/incremental_helper.sh
+++ ukhas/incremental_helper.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 touch /tmp/ukhas-dokuwiki-incremental-start
-tar c /srv/ukhas-data --newer /srv/ukhas-data/incremental-stamp
+tar c /srv/ukhas-data \
+    --newer /srv/ukhas-data/incremental-stamp \
+    --exclude=/srv/ukhas-data/data/cache
 touch /srv/ukhas-data/incremental-stamp --reference=/tmp/ukhas-dokuwiki-incremental-start
 rm /tmp/ukhas-dokuwiki-incremental-start
