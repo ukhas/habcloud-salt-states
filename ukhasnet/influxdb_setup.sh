7d8ced9f53612b37a9f653722fc7bbf839d9c734
Adam Greig <adam@adamgreig.com>
2016-03-24 01:37:01 +0000

    fix command name error and config file error

--- ukhasnet/influxdb_setup.sh
+++ ukhasnet/influxdb_setup.sh
@@ -4,22 +4,22 @@
 {% set databases = pillar['ukhasnet']['influxdb']['databases'] %}
 
 # Create admin user
-influxdb -username admin -password {{ admin_password }} \
+influx -username admin -password {{ admin_password }} \
     -execute "CREATE USER admin WITH PASSWORD '{{ admin_password }}' WITH ALL PRIVILEGES"
 
 # Create each database
 {% for database in databases %}
-influxdb -username admin -password {{ admin_password }} \
+influx -username admin -password {{ admin_password }} \
     -execute "CREATE DATABASE {{ database }}"
 {% endfor %}
 
 # Create each non-admin user and grant them permissions
 {% for user, data in users.items() %}
-influxdb -username admin -password {{ admin_password }} \
+influx -username admin -password {{ admin_password }} \
     -execute "CREATE USER {{ user }} WITH PASSWORD '{{ data.password }}'"
 
 {% for database, level in data.grants.items() %}
-influxdb -username admin -password {{ admin_password }} \
+influx -username admin -password {{ admin_password }} \
     -execute "GRANT {{ level }} ON {{ database }} TO {{ user }}"
 {% endfor %}
 
